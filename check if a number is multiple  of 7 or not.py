2bf90f71a65b69309481682b96634ec45898c87c
KISHOR SUTHAR <sutharkishor600@gmail.com>
2025-07-08 17:25:27 +0530

    python

---
+++ check if a number is multiple  of 7 or not.py
@@ -0,0 +1,5 @@
+num=float(input("Enter a number here :"))
+if(num%7==0):
+    print("the number is multiple by 7")
+else:
+    print("the number is not multiple by 7")
---
+++ sum of number is 1 to N using.py
@@ -0,0 +1,7 @@
+num=int(input("Enter the number :"))
+sum=0
+i=1
+for i in range(1,num+1):
+    sum+=i
+    i+=1
+print("the total of number is ",sum)
